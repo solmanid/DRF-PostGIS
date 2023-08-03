78173c52af2ba14b8d05e984f21ad3f476503689
mohammad <soltanim00800@gmail.com>
2023-08-03 08:36:09 +0330

    accountant done

--- accountants/urls.py
+++ accountants/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('profile/<int:id>', views.AccountantProfileView.as_view(), name='accountant_profile'),
     path('accepted/', views.AcceptedList.as_view(), name='accountant_mark'),
     path('payment/', views.AddPayment.as_view(), name='payment'),
-    # path('payment-factor/', views.export_factor, name='factor'),
+    path('payment-factor/', views.export_factor, name='factor'),
 ]
--- accountants/views.py
+++ accountants/views.py
@@ -1,7 +1,14 @@
 # Django build-in
+import datetime
+from io import BytesIO
+
+from django.db.models import Sum
 from django.http import HttpRequest, HttpResponse
+# Third party
+from reportlab.pdfgen import canvas
 from rest_framework import generics
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 # DRF
 from rest_framework.views import APIView
@@ -13,14 +20,12 @@ from .permissions import IsAccountantUser
 from .serializers import AccountantProfileSerializer, AcceptedListSerializer, AddPaymentSerializers
 
 
-# Third party
-
-
 class AccountantProfileView(APIView):
     """
     This view only show accountant user information
     """
     serializer_class = AccountantProfileSerializer
+    permission_classes = [IsAccountantUser, ]
 
     def get(self, request: HttpRequest, id):
         user = Accountant.objects.filter(id=id).first()
@@ -82,10 +87,10 @@ class AcceptedList(generics.ListAPIView):
 
 
 class AddPayment(APIView):
-    def get(self, request: HttpRequest):
-        query = AcceptedPlace.objects.filter(is_paid=False, action=AcceptedPlace.Status.accepted)
-        ser_data = AcceptedListSerializer(instance=query, many=True)
-        return Response(data=ser_data.data, status=status.HTTP_200_OK)
+    # def get(self, request: HttpRequest):
+    #     query = AcceptedPlace.objects.filter(is_paid=False, action=AcceptedPlace.Status.accepted)
+    #     ser_data = AcceptedListSerializer(instance=query, many=True)
+    #     return Response(data=ser_data.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         ser_data = AddPaymentSerializers(data=request.data, context={'request': request})
@@ -108,4 +113,50 @@ class AddPayment(APIView):
             return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET'])
+@permission_classes([IsAccountantUser])
+def export_factor(request):
+    buffer = BytesIO()
 
+    c = canvas.Canvas(buffer)
+    factors = PaymentMark.objects.all()
+    lines = []
+    text_obj = c.beginText()
+    text_obj.setTextOrigin(50, 800)
+    text_obj.setFont('Helvetica', 14)
+
+    for factor in factors:
+        lines.append(f"Accountant:              {factor.accountant.username}")
+        lines.append(" ")
+        lines.append(f"Level:                       {factor.accept_mark.level}")
+        lines.append(" ")
+        lines.append(f"Supervisor:               {factor.accept_mark.supervisor.username}")
+        lines.append(" ")
+        lines.append(f"Created mark by:      {factor.accept_mark.mark.user.username}")
+        lines.append(" ")
+        lines.append(f"Price:                        {str(factor.price)}")
+        lines.append(" ")
+        lines.append(f"Date:                         {str(factor.created.date())}")
+        lines.append(" ")
+        lines.append(f"Time:                         {str(factor.created.time())}")
+        lines.append(" ")
+        lines.append('_____________________________________________________________')
+    for line in lines:
+        text_obj.textLine(line)
+    total = PaymentMark.objects.all().aggregate(total=Sum('price'))
+    text_obj.textLine(" ")
+    text_obj.textLine(f"Total: {total['total']}")
+    c.drawText(text_obj)
+
+    c.showPage()
+    c.save()
+
+    buffer.seek(0)
+    pdf_content = buffer.getvalue()
+
+    buffer.close()
+
+    response = HttpResponse(pdf_content, content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{datetime.datetime.now()}.pdf"'
+
+    return response
